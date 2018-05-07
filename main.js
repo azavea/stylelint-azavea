84ee0d65aa09ae6604001b877ba8d3651910a7d4
Scott Lederer <slederer@azavea.com>
2018-05-07 13:05:03 -0400

    Allow empty source files

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ module.exports = {
         "max-nesting-depth": [3, {
             "ignoreAtRules": "/.*/"
         }],
+        "no-empty-source": false,
         "selector-max-id": 0,
         "selector-max-type": 0,
         "shorthand-property-no-redundant-values": true,
